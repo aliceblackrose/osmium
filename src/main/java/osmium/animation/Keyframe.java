9dbe8569cf82f2cf178a9440cb4b1de5e1ccf813
cybellereaper <79605349+cybellereaper@users.noreply.github.com>
2026-08-21 01:57:47 -0500

    feat: retain Blockbench bezier handles in keyframes

--- src/main/java/osmium/animation/Keyframe.java
+++ src/main/java/osmium/animation/Keyframe.java
@@ -2,8 +2,31 @@ package osmium.animation;
 
 import osmium.math.Vec3;
 
-public record Keyframe(double time, Vec3 pre, Vec3 post, Interpolation interpolation) {
-  public Keyframe(double t, Vec3 v, Interpolation i) {
-    this(t, v, v, i);
+public record Keyframe(
+    double time,
+    Vec3 pre,
+    Vec3 post,
+    Interpolation interpolation,
+    Vec3 bezierLeftTime,
+    Vec3 bezierRightTime,
+    Vec3 bezierLeftValue,
+    Vec3 bezierRightValue) {
+  public Keyframe {
+    bezierLeftTime = defaultVector(bezierLeftTime);
+    bezierRightTime = defaultVector(bezierRightTime);
+    bezierLeftValue = defaultVector(bezierLeftValue);
+    bezierRightValue = defaultVector(bezierRightValue);
+  }
+
+  public Keyframe(double time, Vec3 pre, Vec3 post, Interpolation interpolation) {
+    this(time, pre, post, interpolation, Vec3.ZERO, Vec3.ZERO, Vec3.ZERO, Vec3.ZERO);
+  }
+
+  public Keyframe(double time, Vec3 value, Interpolation interpolation) {
+    this(time, value, value, interpolation);
+  }
+
+  private static Vec3 defaultVector(Vec3 value) {
+    return value == null ? Vec3.ZERO : value;
   }
 }
