aa9c474f72cbd5eaa18525e9a4cfa86648e5864b
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-08-28 04:14:27 -0500

    Cover Blockbench model geometry basis conversion

--- src/test/java/osmium/math/TransformsTest.java
+++ src/test/java/osmium/math/TransformsTest.java
@@ -3,6 +3,7 @@ package osmium.math;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.joml.Quaternionf;
+import org.joml.Vector3f;
 import org.junit.jupiter.api.Test;
 
 final class TransformsTest {
@@ -25,5 +26,16 @@ final class TransformsTest {
     assertEquals(expected.y, actual.y, EPSILON);
     assertEquals(expected.z, actual.z, EPSILON);
     assertEquals(expected.w, actual.w, EPSILON);
+  }
+
+  @Test
+  void axisConversionRotatesLocalModelGeometryIntoRuntimeBasis() {
+    Vector3f position = new Vector3f(1, 2, 3);
+
+    Transforms.axisConversionRotation().transform(position);
+
+    assertEquals(-1, position.x, EPSILON);
+    assertEquals(2, position.y, EPSILON);
+    assertEquals(-3, position.z, EPSILON);
   }
 }
