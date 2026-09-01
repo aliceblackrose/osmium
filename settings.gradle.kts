2a7dfe6b5185f69fa311f8f51d816ad81684b433
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-08-31 19:24:44 -0500

    Allow Paper userdev repository injection

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,8 +7,6 @@ pluginManagement {
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-
     repositories {
         mavenCentral()
         maven("https://repo.papermc.io/repository/maven-public/")
