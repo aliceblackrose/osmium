184428d9627c15a39fa56e3ae7ecd7f0f4ec918d
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-08-31 19:45:47 -0500

    Share stable display transform decomposition

--- src/main/java/osmium/render/DisplayTransform.java
+++ src/main/java/osmium/render/DisplayTransform.java
@@ -2,14 +2,10 @@ package osmium.render;
 
 import org.bukkit.util.Transformation;
 import org.joml.Matrix4f;
-import org.joml.Quaternionf;
-import org.joml.Vector3f;
+import osmium.math.TransformDecomposition;
 
 /** Chooses the stable display representation for a local model transform. */
 final class DisplayTransform {
-  private static final float SHEAR_EPSILON = 1.0E-4F;
-  private static final float MINIMUM_BASIS_LENGTH_SQUARED = 1.0E-12F;
-
   private DisplayTransform() {}
 
   /**
@@ -18,52 +14,15 @@ final class DisplayTransform {
    * shear.
    */
   static boolean canUseDirectTrs(Matrix4f matrix) {
-    float xLengthSquared =
-        matrix.m00() * matrix.m00() + matrix.m01() * matrix.m01() + matrix.m02() * matrix.m02();
-    float yLengthSquared =
-        matrix.m10() * matrix.m10() + matrix.m11() * matrix.m11() + matrix.m12() * matrix.m12();
-    float zLengthSquared =
-        matrix.m20() * matrix.m20() + matrix.m21() * matrix.m21() + matrix.m22() * matrix.m22();
-
-    if (xLengthSquared <= MINIMUM_BASIS_LENGTH_SQUARED
-        || yLengthSquared <= MINIMUM_BASIS_LENGTH_SQUARED
-        || zLengthSquared <= MINIMUM_BASIS_LENGTH_SQUARED) {
-      return false;
-    }
-
-    float xy =
-        matrix.m00() * matrix.m10() + matrix.m01() * matrix.m11() + matrix.m02() * matrix.m12();
-    float xz =
-        matrix.m00() * matrix.m20() + matrix.m01() * matrix.m21() + matrix.m02() * matrix.m22();
-    float yz =
-        matrix.m10() * matrix.m20() + matrix.m11() * matrix.m21() + matrix.m12() * matrix.m22();
-
-    if (!approximatelyOrthogonal(xy, xLengthSquared, yLengthSquared)
-        || !approximatelyOrthogonal(xz, xLengthSquared, zLengthSquared)
-        || !approximatelyOrthogonal(yz, yLengthSquared, zLengthSquared)) {
-      return false;
-    }
-
-    return determinant3x3(matrix) > 0;
+    return TransformDecomposition.canUseDirectTrs(matrix);
   }
 
   static Transformation directTrs(Matrix4f matrix) {
-    Vector3f translation = matrix.getTranslation(new Vector3f());
-    Quaternionf rotation = matrix.getUnnormalizedRotation(new Quaternionf()).normalize();
-    Vector3f scale = matrix.getScale(new Vector3f());
-    return new Transformation(translation, rotation, scale, new Quaternionf());
-  }
-
-  private static boolean approximatelyOrthogonal(
-      float dot, float firstLengthSquared, float secondLengthSquared) {
-    double maximumDot =
-        SHEAR_EPSILON * Math.sqrt((double) firstLengthSquared * secondLengthSquared);
-    return Math.abs(dot) <= maximumDot;
-  }
-
-  private static float determinant3x3(Matrix4f matrix) {
-    return matrix.m00() * (matrix.m11() * matrix.m22() - matrix.m21() * matrix.m12())
-        - matrix.m10() * (matrix.m01() * matrix.m22() - matrix.m21() * matrix.m02())
-        + matrix.m20() * (matrix.m01() * matrix.m12() - matrix.m11() * matrix.m02());
+    TransformDecomposition.Components components = TransformDecomposition.direct(matrix);
+    return new Transformation(
+        components.translation(),
+        components.leftRotation(),
+        components.scale(),
+        components.rightRotation());
   }
 }
