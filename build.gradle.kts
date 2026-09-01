9bca69ef4570a4471cebc4e980ef59c187cbcf37
aliceblackrose <79605349+aliceblackrose@users.noreply.github.com>
2026-08-31 19:17:16 -0500

    Compile against Paper NMS with userdev

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     java
 
+    id("io.papermc.paperweight.userdev") version "2.0.0-beta.21"
     id("com.diffplug.spotless") version "8.4.0"
     id("net.ltgt.errorprone") version "5.1.0"
 }
@@ -15,11 +16,10 @@ java {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:26.1.2.build.+")
+    paperweight.paperDevBundle("26.1.2.build.+")
 
     errorprone("com.google.errorprone:error_prone_core:2.49.0")
 
-    testImplementation("io.papermc.paper:paper-api:26.1.2.build.+")
     testImplementation(platform("org.junit:junit-bom:5.13.4"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
